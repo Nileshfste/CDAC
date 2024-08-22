4a1f0b9c2c7bb8fc133d679dab6f36e74972c502
Nileshfste <149589003+Nileshfste@users.noreply.github.com>
2024-08-22 14:09:15 +0530

    Add files via upload

---
+++ LOGIC BUILDING/ASSIGNMENTS/ASSIGNMENT 01/goodmorning.java
@@ -0,0 +1,14 @@
+public class goodmorning  
+ {
+    public static void main(String[] args)  
+    {
+       
+        int hour = 10;
+
+       
+        if (hour >= 5 && hour < 12) 
+         {
+            System.out.println("Good Morning!");
+         }
+    }
+  }
---
+++ LOGIC BUILDING/ASSIGNMENTS/ASSIGNMENT 01/negitive.java
@@ -0,0 +1,14 @@
+public class negitive  
+ {
+    public static void main(String[] args)  
+    {
+       
+        int num = 10;
+
+       
+        if (num< 0) 
+         {
+            System.out.println("negitive");
+         }
+    }
+  }
